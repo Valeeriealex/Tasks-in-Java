f0aecad4b81ca2b6a96d0bc2bfa83c25358538c4
Vlerie <Valerie.Basaly@mail.ru>
2023-02-14 21:21:01 +0300

    Task_2

--- Triangular number/triangular_number.java
+++ Triangular number/triangular_number.java
@@ -1,5 +1,26 @@
-package Triangular number;
+// Написать программу вычисления n-ого треугольного числа.
+
+import java.util.Scanner;
 
 public class triangular_number {
-    
+    // Метод получения числа от пользователя
+    public static void main (String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int a = Scanner.nextInt();
+        Scanner.close();
+        System.out.println(a);
+        //System.out.println("Треугольное число = " + Triangle(a));
+    }
+    // Метод вычисления треугольного числа
+    public static int Triangle(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return Triangle(num - 1) + num;
+    }
+
+    public static String print (int a, int num) {
+        return ("Треугольное число = " + Triangle(a));
+    }
 }
