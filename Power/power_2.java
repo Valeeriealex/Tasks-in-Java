d1d7b52c3bf6520064bb5ee1618cfd58c11ca429
Vlerie <Valerie.Basaly@mail.ru>
2023-02-20 20:27:06 +0300

    Task_2

--- Power/power_2.java
+++ Power/power_2.java
@@ -1,26 +1,22 @@
 package Power;
 
 import java.util.Scanner;
-import java.io.*;
 
 public class power_2 {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {                                     //главный метод, принимающий аргументы
         System.out.print("Введите число, которое надо возвести в степень: "); //пользователь вводит число
 		Scanner scan1 = new Scanner(System.in);                                 //сканнер считывает число
 		int number = scan1.nextInt();                                           //в переменную помещается отсканированое число
+    
 
 		System.out.print("Введите степень: ");                                //аналогично
 		Scanner scan2 = new Scanner(System.in);
 		int power = scan2.nextInt();
-    }
+        
+        System.out.println("Число " + number + " в степени " + power + " равно " + Math.pow(number, power)); //возведение в степень
 
-    public static int NumberToPower (number, power) 
-    { 
-        if (power == 1) {
-            return number;
-        } else {
-            return number * pow(number, power - 1);
-        }
+        scan1.close();
+        scan2.close();
     }
 }
+
